ef27b49f59d2bf6ccf27566f7e6e5ee747ad6302
tripleseven190504 <tripleseven190504@gmail.com>
2024-05-16 06:50:42 +0700

    fix cache not working

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
 self.addEventListener("install", function (event) {
-	event.waitUntil(
-		caches.open("v1").then(function (cache) {
-			return cache.addAll([
-				"/",
-				"/css/style.css",
-				"/img/apple-icon-180.png",
-				"/index.html",
-			]);
-		}),
-	);
+  event.waitUntil(
+    caches.open("v1").then(function (cache) {
+      return cache.addAll([
+        "/techcombank/",
+        "/techcombank/css/style.css",
+        "/techcombank/img/apple-icon-180.png",
+        "/techcombank/index.html",
+      ]);
+    }),
+  );
 });
 
 self.addEventListener("fetch", function (event) {
-	event.respondWith(
-		caches.match(event.request).then(function (response) {
-			return response || fetch(event.request);
-		}),
-	);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }),
+  );
 });
